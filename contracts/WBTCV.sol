45ad9266caab1062487daa522c0a2164ed74028d
mgrychow <maciej.grychowski@onet.eu>
2021-04-26 10:15:24 +0200

    blocking transfers from specific user by contract owner

--- contracts/WBTCV.sol
+++ contracts/WBTCV.sol
@@ -6,7 +6,15 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract WBTCV is ERC20Burnable, Ownable
 {
+    mapping(address => bool) public blocked;
+
     constructor() ERC20("Wrapped Bitcoin Vault", "WBTCV") {
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(blocked[msg.sender] == false, 'User is blocked');
+        _transfer(_msgSender(), recipient, amount);
+        return true;
     }
 
     function mint(address addr, uint256 amount) public onlyOwner{
@@ -15,5 +23,13 @@ contract WBTCV is ERC20Burnable, Ownable
 
     function burn(uint256 amount) public override onlyOwner{
         super.burn(amount);
+    }
+
+    function blockUser(address userAddress) public onlyOwner{
+        blocked[userAddress] = true;
+    }
+
+    function unblockUser(address userAddress) public onlyOwner{
+        blocked[userAddress] = false;
     }
 }
--- test/wbtcv.js
+++ test/wbtcv.js
@@ -1,4 +1,7 @@
 const WBTCV = artifacts.require("WBTCV");
+const {
+  expectRevert, // Assertions for transactions that should fail
+} = require('@openzeppelin/test-helpers');
 
 contract('WBTCV', (accounts) => {
   it('should have 0 coins as initial amount', async () => {
@@ -19,6 +22,36 @@ contract('WBTCV', (accounts) => {
     await instance.burn(10);
     balance = await instance.balanceOf.call(accounts[0]);
     assert.equal(web3.utils.toHex(balance.valueOf()), web3.utils.toHex('10'), "10 wasn't the amount after burn");
+  });
+
+   it('should not increase value after mint from not owner', async () => {
+    const instance = await WBTCV.deployed();
+    await expectRevert(instance.mint(accounts[0], 20, {from: accounts[1]}), 'Ownable: caller is not the owner');
+  });
+
+  it('should perform succesful transfer', async () => {
+    const instance = await WBTCV.deployed();
+    await instance.transfer(accounts[1], 3);
+    balance0 = await instance.balanceOf.call(accounts[0]);
+    balance1 = await instance.balanceOf.call(accounts[1]);
+    assert.equal(web3.utils.toHex(balance0.valueOf()), web3.utils.toHex('7'), "7 wasn't the balance after transfer");
+    assert.equal(web3.utils.toHex(balance1.valueOf()), web3.utils.toHex('3'), "3 wasn't the balance after transfer");
+  });
+
+  it('should not allow blocked user to make transfer', async () => {
+    const instance = await WBTCV.deployed();
+    await instance.blockUser(accounts[1])
+    await expectRevert(instance.transfer(accounts[2], 2, {from: accounts[1]}), 'User is blocked');
+  });
+
+  it('should allow unblocked user to make transfer', async () => {
+    const instance = await WBTCV.deployed();
+    await instance.unblockUser(accounts[1]);
+    await instance.transfer(accounts[2], 1, {from: accounts[1]});
+    balance1 = await instance.balanceOf.call(accounts[1]);
+    balance2 = await instance.balanceOf.call(accounts[2]);
+    assert.equal(web3.utils.toHex(balance1.valueOf()), web3.utils.toHex('2'), "2 wasn't the balance after transfer");
+    assert.equal(web3.utils.toHex(balance2.valueOf()), web3.utils.toHex('1'), "1 wasn't the balance after transfer");
   });
 
 });
