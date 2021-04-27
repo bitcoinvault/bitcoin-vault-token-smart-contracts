4117515cd5358b785f50a2d0ede56115b56fbd07
mgrychow <maciej.grychowski@onet.eu>
2021-04-27 08:25:35 +0200

    sending and querying alert (secure) transfers

--- contracts/WBTCV.sol
+++ contracts/WBTCV.sol
@@ -4,9 +4,22 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
+struct Alert{
+    address recipient;
+    uint256 amount;
+    address cancelAccount;
+    uint blockNumber;
+}
+
 contract WBTCV is ERC20Burnable, Ownable
 {
     mapping(address => bool) public blocked;
+    mapping(address => Alert[]) public incomingAlerts;
+    mapping (address => uint256) private _balancesLockedToAlerts;
+
+    uint public constant ALERT_BLOCK_WAIT = 240;
+
+    event SentAlert(address sender, address recipient, uint256 amount, address cancelAccount);
 
     constructor() ERC20("Wrapped Bitcoin Vault", "WBTCV") {
     }
@@ -37,5 +50,51 @@ contract WBTCV is ERC20Burnable, Ownable
 
     function unblockUser(address userAddress) public onlyOwner{
         blocked[userAddress] = false;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return super.balanceOf(account) - _balancesLockedToAlerts[account];
+    }
+
+    function transferAlert(address recipient, uint256 amount, address cancelAccount) public{
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(balanceOf(msg.sender) >= amount, "ERC20: transfer amount exceeds balance");
+
+        _balancesLockedToAlerts[msg.sender] = _balancesLockedToAlerts[msg.sender] + amount;
+        incomingAlerts[recipient].push(Alert(recipient, amount, cancelAccount, block.number));
+
+        emit SentAlert(msg.sender, recipient, amount, cancelAccount);
+    }
+
+    function getIncomingAlerts(address addr) public view returns (Alert[] memory){
+        Alert[] memory l_incomingAlerts = new Alert[](incomingAlerts[addr].length);
+        for (uint i = 0; i < incomingAlerts[addr].length; i++) {
+            Alert storage alert = incomingAlerts[addr][i];
+            l_incomingAlerts[i] = alert;
+        }
+        return l_incomingAlerts;
+    }
+
+    function getReadyAlerts(address addr) public view returns (Alert[] memory){
+        uint j = 0;
+        for (uint i = 0; i < incomingAlerts[addr].length; i++) {
+            Alert storage alert = incomingAlerts[addr][i];
+            if(alert.blockNumber + ALERT_BLOCK_WAIT <= block.number)
+                j++;
+        }
+        Alert[] memory l_incomingAlerts0;
+        if(j == 0)
+            return l_incomingAlerts0;
+        Alert[] memory l_incomingAlerts = new Alert[](j);
+        j = 0;
+        for (uint i = 0; i < incomingAlerts[addr].length; i++) {
+            Alert storage alert = incomingAlerts[addr][i];
+            if(alert.blockNumber + ALERT_BLOCK_WAIT <= block.number)
+            {
+                l_incomingAlerts[j] = alert;
+                j++;
+            }
+        }
+        return l_incomingAlerts;
     }
 }
--- test/wbtcv.js
+++ test/wbtcv.js
@@ -1,9 +1,13 @@
 const WBTCV = artifacts.require("WBTCV");
 const {
-  expectRevert, // Assertions for transactions that should fail
+    time,
+    expectRevert, // Assertions for transactions that should fail
 } = require('@openzeppelin/test-helpers');
 
 contract('WBTCV', (accounts) => {
+
+/// TOKEN FEATURES
+
   it('should have 0 coins as initial amount', async () => {
     const instance = await WBTCV.deployed();
     balance = await instance.balanceOf.call(accounts[0]);
@@ -24,7 +28,7 @@ contract('WBTCV', (accounts) => {
     assert.equal(web3.utils.toHex(balance.valueOf()), web3.utils.toHex('10'), "10 wasn't the amount after burn");
   });
 
-   it('should not increase value after mint from not owner', async () => {
+  it('should not increase value after mint from not owner', async () => {
     const instance = await WBTCV.deployed();
     await expectRevert(instance.mint(accounts[0], 20, {from: accounts[1]}), 'Ownable: caller is not the owner');
   });
@@ -37,6 +41,8 @@ contract('WBTCV', (accounts) => {
     assert.equal(web3.utils.toHex(balance0.valueOf()), web3.utils.toHex('7'), "7 wasn't the balance after transfer");
     assert.equal(web3.utils.toHex(balance1.valueOf()), web3.utils.toHex('3'), "3 wasn't the balance after transfer");
   });
+
+/// BLOCKING FEATURE
 
   it('should not allow blocked user to make transfer', async () => {
     const instance = await WBTCV.deployed();
@@ -72,6 +78,38 @@ contract('WBTCV', (accounts) => {
     assert.equal(web3.utils.toHex(balance0.valueOf()), web3.utils.toHex('5'), "5 wasn't the balance after transfer");
     assert.equal(web3.utils.toHex(balance1.valueOf()), web3.utils.toHex('2'), "2 wasn't the balance after transfer");
     assert.equal(web3.utils.toHex(balance2.valueOf()), web3.utils.toHex('3'), "3 wasn't the balance after transfer");
+  });
+
+/// ALERT FEATURE
+
+  it('should perform succesful transfer with alert', async () => {
+    const instance = await WBTCV.deployed();
+
+    await instance.transferAlert(accounts[1], 1, accounts[2]);
+    balance0 = await instance.balanceOf.call(accounts[0]);
+    balance1 = await instance.balanceOf.call(accounts[1]);
+    balance2 = await instance.balanceOf.call(accounts[2]);
+    assert.equal(web3.utils.toHex(balance0.valueOf()), web3.utils.toHex('4'), "4 wasn't the balance after transfer");
+    assert.equal(web3.utils.toHex(balance1.valueOf()), web3.utils.toHex('2'), "2 wasn't the balance after transfer");
+    assert.equal(web3.utils.toHex(balance2.valueOf()), web3.utils.toHex('3'), "3 wasn't the balance after transfer");
+
+    incomingAlerts = await instance.getIncomingAlerts(accounts[1]);
+    assert.equal(incomingAlerts.length, 1, "incoming alerts length not 1");
+    assert.equal(incomingAlerts[0].recipient, accounts[1], "incoming alerts");
+    assert.equal(incomingAlerts[0].amount, 1, "incoming alerts");
+    assert.equal(incomingAlerts[0].cancelAccount, accounts[2], "incoming alerts");
+    assert.equal(web3.utils.toHex(incomingAlerts[0].blockNumber), web3.utils.toHex(await time.latestBlock().valueOf()), "incoming alerts");
+
+    incomingAlerts = await instance.getReadyAlerts(accounts[1]);
+    assert.equal(incomingAlerts.length, 0, "ready alerts length not 0");
+    for(i = 0; i < 240; i++)
+        await time.advanceBlock();
+    incomingAlerts = await instance.getReadyAlerts(accounts[1]);
+    assert.equal(incomingAlerts.length, 1, "ready alerts length not 1");
+    assert.equal(incomingAlerts[0].recipient, accounts[1], "ready alerts");
+    assert.equal(incomingAlerts[0].amount, 1, "ready alerts");
+    assert.equal(incomingAlerts[0].cancelAccount, accounts[2], "ready alerts");
+    assert.equal(web3.utils.toHex(incomingAlerts[0].blockNumber), web3.utils.toHex(await time.latestBlock().valueOf()) - web3.utils.toHex(240), "ready alerts");
   });
 
 });
